1f9f87eab56ffe308fa00e4a6f73eccead07a288
mathilde ganancia <mathilde.ganancia@gmail.com>
2023-01-30 11:31:06 +0100

    make it better

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,9 @@ Bundler.require(*Rails.groups)
 module Gstudio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.
+    config.assets.initialize_on_precompile = false
+
 
     # Configuration for the application, engines, and railties goes here.
     #
